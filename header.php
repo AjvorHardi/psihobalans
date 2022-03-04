0be0ed396b23df127d3d1fe6c01a42f32dc2a9b0
AjvorHardi <mravsteen@gmail.com>
2022-03-04 12:16:34 +0100

    Separate header

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Psihobalans</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400&display=swap');</style>
+</head>
+
+<body>
+    <nav>   
+        <div class="container">
+            <ul class="zaglavlje">
+                    <li><a href=""><i class="fas fa-envelope"></i></a>psihobalans.ns@gmail.com</li>
+                    <li><a href=""><i class="fas fa-map-marker-alt"></i></a>Djure Jaksica 15, 21000 Novi Sad, Srbija</li>
+            </ul>
+            <div class="sredina">
+                <a href="index.php" class="logo">
+                    <img src="img/Logo-Psihobalans-retina.png" alt="">
+                </a>
+                <div class="zakazivanje">
+                    <p>Pozovite: 064 2636 513</p>
+                    <button><a href="https://www.picktime.com/efe4e723-3524-4904-91dd-4d6af8c3be10">Zakazite termin</a></button>
+                </div>
+            </div>
+            <ul class="linkovi">
+                <li><a href="index.php">Pocetna</a></li>
+                <li><a href="moj-pristup.php">Moj Pristup</a></li>
+                <li><a href="usluge.php">Usluge</a></li>
+                <li><a href="o-meni.php">O meni</a></li>
+                <li><a href="clanci.php">Clanci</a></li>
+                <li><a href="kontakt-svetlana-prokic.php">Kontakt</a></li>
+            </ul>
+        </div>
+    </nav>
--- moj-pristup.php
+++ moj-pristup.php
@@ -1,6 +1,6 @@
 <?php include 'header.php' ; ?>
 
-    <h1>Homepage</h1>
+    <h1>Moj Pristup</h1>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="js/owl.carousel.min.js"></script>
