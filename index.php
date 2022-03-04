a2d2453a440fd1f6a6ba1dce509d84da69fc9f5b
AjvorHardi <mravsteen@gmail.com>
2022-03-04 12:12:46 +0100

    Finish Header

--- index.php
+++ index.php
@@ -9,11 +9,37 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400&display=swap');</style>
 </head>
 
 <body>
 
-<h1>PSIHOBALANS</h1>
+
+    <nav>   
+        <div class="container">
+            <ul class="zaglavlje">
+                    <li><a href=""><i class="fas fa-envelope"></i></a>psihobalans.ns@gmail.com</li>
+                    <li><a href=""><i class="fas fa-map-marker-alt"></i></a>Djure Jaksica 15, 21000 Novi Sad, Srbija</li>
+            </ul>
+            <div class="sredina">
+                <a href="index.php" class="logo">
+                    <img src="img/Logo-Psihobalans-retina.png" alt="">
+                </a>
+                <div class="zakazivanje">
+                    <p>Pozovite: 064 2636 513</p>
+                    <button><a href="https://www.picktime.com/efe4e723-3524-4904-91dd-4d6af8c3be10">Zakazite termin</a></button>
+                </div>
+            </div>
+            <ul class="linkovi">
+                <li><a href="index.php">Pocetna</a></li>
+                <li><a href="moj-pristup.php">Moj Pristup</a></li>
+                <li><a href="usluge.php">Usluge</a></li>
+                <li><a href="o-meni.php">O meni</a></li>
+                <li><a href="clanci.php">Clanci</a></li>
+                <li><a href="kontakt-svetlana-prokic.php">Kontakt</a></li>
+            </ul>
+        </div>
+    </nav>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="js/owl.carousel.min.js"></script>
